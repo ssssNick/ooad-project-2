1028499172b27fefb2e8b630b01908038489c651
Nick Brubaker <brubakernick@gmail.com>
2020-01-24 11:25:32 -0700

    Added Pachyderm, Feline, and Canine Classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-// Author: Nick Brubaker
+// Authors: Nick Brubaker...
+// References: 
+// 		Printing the name of a class - https://stackoverflow.com/questions/6271417/java-get-the-current-class-name
 
 public class Main
 {
@@ -6,26 +8,78 @@ public class Main
 	public static void main(String[] args)
 	{
 		
-		Animal a = new Animal("Allen");
-		a.sleep();
-
+		Canine a = new Canine("Allen");
+		a.roam();
+		
 	}
 
 }
 
 class Animal 
 {
-	
+
+	private String type;
 	private String name;
 	
 	public Animal(String newName)
 	{
+		this.type = this.getClass().getName();
 		this.name = newName;
+	}
+	
+	public void doSomething(String thing)
+	{
+		System.out.println(this.name + " the " + this.type + " started " + thing);		
 	}
 	
 	public void sleep() 
 	{
-		System.out.println(this.name + " the Animal is sleeping");
+		this.doSomething("sleeping");
+	}
+
+}
+
+class Pachyderm extends Animal 
+{
+	
+	public Pachyderm(String newName)
+	{
+		super(newName);
+	}
+	
+		public void roam() 
+	{
+		this.doSomething("roaming noisily");
+	}
+
+}
+
+class Feline extends Animal 
+{
+	
+	public Feline(String newName)
+	{
+		super(newName);
+	}
+	
+		public void roam() 
+	{
+		this.doSomething("roaming quietly");
+	}
+
+}
+
+class Canine extends Animal 
+{
+	
+	public Canine(String newName)
+	{
+		super(newName);
+	}
+	
+		public void roam() 
+	{
+		this.doSomething("roaming happily");
 	}
 
 }
