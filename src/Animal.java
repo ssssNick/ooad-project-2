944c1892568447b3a4f9fea88f4eabbe2300928a
Dominic Doty <doty.dominic@gmail.com>
2020-01-28 15:37:24 -0700

    Added other methods to all animals with varying levels of inheritance. Made superclasses abstract

--- src/Animal.java
+++ src/Animal.java
@@ -8,239 +8,233 @@
 
 import java.util.concurrent.ThreadLocalRandom;
 
-class Animal 
-{
+abstract class Animal {
 
 	private String type;
 	private String name;
-	
-	public Animal(String newName)
-	{
+
+	public Animal(String newName) {
 		this.type = this.getClass().getName();
 		this.name = newName;
 	}
-	
-	public void doSomething(String thing)
-	{
-		System.out.println(this.name + " the " + this.type + " started " + thing);		
+
+	public void doSomething(String thing) {
+		System.out.println(this.name + " the " + this.type + " started " + thing);
 	}
-	
-	public void sleep() 
-	{
+
+	public void sleep() {
 		this.doSomething("sleeping");
 	}
 
+	public void wakeUp() {
+		this.doSomething("waking up");
+	}
+
+	abstract public void makeNoise();
+
+	abstract public void eat();
+
+	abstract public void roam();
 }
 
-class Pachyderm extends Animal 
-{
-	
-	public Pachyderm(String newName)
-	{
+abstract class Pachyderm extends Animal {
+
+	public Pachyderm(String newName) {
 		super(newName);
 	}
-	
-		public void roam() 
-	{
+
+	public void makeNoise() {
+		this.doSomething("grunting");
+	}
+
+	public void eat() {
+		this.doSomething("eating grass");
+	}
+
+	public void roam() {
 		this.doSomething("roaming noisily");
 	}
 
 }
 
-class Feline extends Animal 
-{
-	
-	public Feline(String newName)
-	{
+abstract class Feline extends Animal {
+
+	public Feline(String newName) {
 		super(newName);
 	}
-	
-		public void roam() 
-	{
-		this.doSomething("roaming quietly");
+
+	public void eat() {
+		this.doSomething("eating poor, poor animals");
+	}
+
+	public void roam() {
+		this.doSomething("stalking quietly");
 	}
 
 }
 
-class Canine extends Animal 
-{
-	
-	public Canine(String newName)
-	{
+abstract class Canine extends Animal {
+
+	public Canine(String newName) {
 		super(newName);
 	}
-	
-		public void roam() 
-	{
+
+	public void makeNoise() {
+		this.doSomething("howling");
+	}
+
+	public void eat() {
+		this.doSomething("eating a squirrel");
+	}
+
+	public void roam() {
 		this.doSomething("roaming happily");
 	}
 
 }
 
-class Hippo extends Pachyderm 
-{
-	
-	public Hippo(String newName)
-	{
+class Hippo extends Pachyderm {
+
+	public Hippo(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("making Hippo sounds");
 	}
 
 }
 
-class Elephant extends Pachyderm 
-{
-	
-	public Elephant(String newName)
-	{
+class Elephant extends Pachyderm {
+
+	public Elephant(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("making Elephant sounds");
 	}
 
+	public void eat() {
+		this.doSomething("eating a tree top");
+	}
+
 }
 
-class Rhino extends Pachyderm 
-{
-	
-	public Rhino(String newName)
-	{
+class Rhino extends Pachyderm {
+
+	public Rhino(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("making Rhino sounds");
 	}
 
+	public void roam() {
+		this.doSomething("digging a midden");
+	}
+
 }
 
-class Tiger extends Feline 
-{
-	
-	public Tiger(String newName)
-	{
+class Tiger extends Feline {
+
+	public Tiger(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("growling");
 	}
 
 }
 
-class Lion extends Feline 
-{
-	
-	public Lion(String newName)
-	{
+class Lion extends Feline {
+
+	public Lion(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("roaring");
 	}
 
 }
 
-class Cat extends Feline 
-{
-	
-	public Cat(String newName)
-	{
+class Cat extends Feline {
+
+	public Cat(String newName) {
 		super(newName);
 	}
 
-	private void iAmaCat()
-	{
+	private void iAmaCat() {
 		int random = ThreadLocalRandom.current().nextInt(0, 6);
 
 		switch (random) {
-			case 0:
-				this.doSomething("to wake up");
-				break;
-			case 1:
-				this.doSomething("meowing");
-				break;
-			case 2:
-				this.doSomething("eating");
-				break;
-			case 3:
-				this.doSomething("prowling");
-				break;
-			case 4:
-				this.doSomething("laying on laptop keyboard while you try to type");
-				break;
-			case 5:
-				this.doSomething("sleeping");
-				break;
-			default:
-				this.doSomething("random number error");
-				break;
+		case 0:
+			this.doSomething("waking up");
+			break;
+		case 1:
+			this.doSomething("meowing");
+			break;
+		case 2:
+			this.doSomething("eating");
+			break;
+		case 3:
+			this.doSomething("prowling");
+			break;
+		case 4:
+			this.doSomething("laying on laptop keyboard while you try to type");
+			break;
+		case 5:
+			this.doSomething("sleeping");
+			break;
+		default:
+			this.doSomething("random number error");
+			break;
 		}
 	}
-	
-	public void wakeup()
-	{
+
+	public void wakeUp() {
 		this.iAmaCat();
 	}
 
-	public void makeNoise()
-	{
+	public void makeNoise() {
 		this.iAmaCat();
 	}
 
-	public void eat()
-	{
+	public void eat() {
 		this.iAmaCat();
 	}
 
-	public void roam()
-	{
+	public void roam() {
 		this.iAmaCat();
 	}
 
-	public void sleep()
-	{
+	public void sleep() {
 		this.iAmaCat();
 	}
 }
 
-class Wolf extends Canine 
-{
-	
-	public Wolf(String newName)
-	{
+class Wolf extends Canine {
+
+	public Wolf(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("making Wolf sounds");
 	}
 
 }
 
-class Dog extends Canine 
-{
-	
-	public Dog(String newName)
-	{
+class Dog extends Canine {
+
+	public Dog(String newName) {
 		super(newName);
 	}
-	
-		public void makeNoise() 
-	{
+
+	public void makeNoise() {
 		this.doSomething("woofing");
 	}
 }
-
